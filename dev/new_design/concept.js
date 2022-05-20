b17da6a429abced28167a61a09f597f61663f8c3
TitouanCH <titouan.azert@wadsa.com>
2022-05-20 17:11:54 +0200

    worked on design

--- dev/new_design/concept.js
+++ dev/new_design/concept.js
@@ -1,17 +1,18 @@
-const canvas = document.getElementById("simulation canvas");
+const canvas = document.getElementById("simulation_canvas");
 
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
 const ctx = canvas.getContext("2d");
-ctx.beginPath();
-ctx.arc(95, 50, 40, 0, 2 * Math.PI);
-ctx.stroke();
-
 
 function windowResize() {
 	canvas.width = window.innerWidth;
-	canvas.height = window.innerHeight;
+	canvas.height = window.innerHeight + 1;
+	
+	ctx.fillStyle = "#333";
+	ctx.fillRect(0, 0, canvas.width, canvas.height);
 };
 
 window.addEventListener('resize', windowResize);
+
+windowResize();
--- dev/new_design/concept.php
+++ dev/new_design/concept.php
@@ -10,11 +10,11 @@
 
 <main id="main">
 
-<div id="online simulator">
-<canvas id="simulation canvas" style=""></canvas>
+<div id="online_simulator">
+<canvas id="simulation_canvas" style=""></canvas>
 </div>
 
-<div id="description">
+<div id="simulation_description">
 
 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas vel tellus sodales, pulvinar velit sit amet, auctor est. Aenean porttitor ipsum ut neque pretium interdum. Maecenas facilisis quis nunc nec commodo. In hac habitasse platea dictumst. Curabitur libero libero, cursus sit amet purus eget, tempus vulputate metus. In sem augue, faucibus vel hendrerit a, gravida vel enim. Phasellus nec odio quam. In cursus nulla a luctus pellentesque. Cras eget ipsum eu est venenatis lobortis ac vel nulla. Aliquam et turpis interdum, eleifend purus et, consectetur lacus. Nulla efficitur accumsan augue vitae molestie. Ut vitae laoreet ante. Nullam dictum a turpis quis tempus.
 
@@ -25,6 +25,10 @@ Vestibulum interdum massa non mi egestas, non pharetra elit dapibus. Sed commodo
 Phasellus vulputate fermentum ultricies. Nulla vitae odio ac nisl congue bibendum. Praesent et sollicitudin lectus, in porta ex. Fusce bibendum diam sed eros sodales consequat. Quisque porttitor scelerisque urna, in mattis tellus ultricies interdum. Nullam sed sodales odio, vel bibendum ligula. Nullam sollicitudin lacinia erat, vulputate facilisis est euismod vel. Mauris lobortis augue enim, a molestie nunc varius id. Nam vitae porttitor neque. Aliquam erat volutpat. Vivamus nunc mauris, mattis vitae luctus ut, vulputate sed nulla. Phasellus bibendum lorem et dolor dignissim, maximus molestie tellus porttitor. Pellentesque iaculis aliquet dignissim. Ut lacinia enim id turpis dapibus pharetra.
 
 Morbi ullamcorper egestas lectus non vehicula. Aliquam pharetra fermentum tristique. Nam venenatis nulla purus, eget gravida erat pharetra laoreet. Nam vulputate tincidunt malesuada. Nunc lorem nulla, ultricies at magna quis, efficitur varius dui. Proin convallis bibendum ex vel auctor. Sed ut purus sit amet neque maximus hendrerit non et ipsum. Morbi eu dictum mauris. Suspendisse volutpat justo eu dolor pellentesque efficitur. Nam consectetur vulputate malesuada. Aliquam id consequat ante, mattis dictum ligula. Aliquam nec congue eros. Donec imperdiet, odio id ultrices porttitor, lacus erat fermentum tortor, eget pulvinar metus dolor a augue. In suscipit egestas ante, id elementum quam ullamcorper non. Nulla vel rhoncus felis. Ut a tristique sapien.
+
+</div>
+
+<div id="more_simulations">
 
 </div>
 
